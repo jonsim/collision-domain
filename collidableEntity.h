4f9a9bb3cb46f271af43efe2fd1c661cd4df7318
david.wybourn <david.wybourn@gmail.com>
2011-11-07 21:03:39 +0000

    collidable entity

---
+++ collidableEntity.h
@@ -0,0 +1,12 @@
+#ifndef COLLIDABLEENTITY_H
+#define COLLIDABLEENTITY_H
+
+class collidableEntity : public drawableEntity
+{
+ 
+public:
+    collidableEntity() {};
+    ~collidableEntity() {};
+}
+
+#endif
