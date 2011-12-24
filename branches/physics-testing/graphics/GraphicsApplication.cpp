3cfd4ac4259c1c1e0f26535a239fffb3913bfe45
jm-98@otterpost.com <jm-98@otterpost.com@876d4c38-226f-9f3a-54bf-000a6e7cd450>
2011-12-24 02:53:35 +0000

    A little more...

--- branches/physics-testing/graphics/GraphicsApplication.cpp
+++ branches/physics-testing/graphics/GraphicsApplication.cpp
@@ -253,8 +253,6 @@ extern "C" {
         // Create application object
         GraphicsApplication app;
 
-        //showWin32Console();
-
         try {
             app.go();
         } catch( Ogre::Exception& e ) {
@@ -272,44 +270,3 @@ extern "C" {
 #ifdef __cplusplus
 }
 #endif
-
-
-
-/*
-void showWin32Console()
-{
-    static const WORD MAX_CONSOLE_LINES = 500;
-    int hConHandle;
-    long lStdHandle;
-    CONSOLE_SCREEN_BUFFER_INFO coninfo;
-    FILE *fp;
-    // allocate a console for this app
-    AllocConsole();
-    // set the screen buffer to be big enough to let us scroll text
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &coninfo);
-    coninfo.dwSize.Y = MAX_CONSOLE_LINES;
-    SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE),
-    coninfo.dwSize);
-    // redirect unbuffered STDOUT to the console
-    lStdHandle = (long)GetStdHandle(STD_OUTPUT_HANDLE);
-    hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-    fp = _fdopen( hConHandle, "w" );
-    *stdout = *fp;
-    setvbuf( stdout, NULL, _IONBF, 0 );
-    // redirect unbuffered STDIN to the console
-    lStdHandle = (long)GetStdHandle(STD_INPUT_HANDLE);
-    hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-    fp = _fdopen( hConHandle, "r" );
-    *stdin = *fp;
-    setvbuf( stdin, NULL, _IONBF, 0 );
-    // redirect unbuffered STDERR to the console
-    lStdHandle = (long)GetStdHandle(STD_ERROR_HANDLE);
-    hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-    fp = _fdopen( hConHandle, "w" );
-    *stderr = *fp;
-    setvbuf( stderr, NULL, _IONBF, 0 );
-    // make cout, wcout, cin, wcin, wcerr, cerr, wclog and clog
-    // point to console as well
-    std::ios::sync_with_stdio();
-}
-*/
