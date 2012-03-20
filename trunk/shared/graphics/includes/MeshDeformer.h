a9d08d26c25fe12a30e80661b4141a01642a82b0
zlatan.bratovic@gmail.com <zlatan.bratovic@gmail.com@876d4c38-226f-9f3a-54bf-000a6e7cd450>
2012-03-20 18:34:15 +0000

    Jamie and Jonny are cunts and they didn't tell me to click the box to add my classes

---
+++ trunk/shared/graphics/includes/MeshDeformer.h
@@ -0,0 +1,44 @@
+#ifndef __MESHDEFORMER_H_
+#define __MESHDEFORMER_H_
+
+/*-------------------- INCLUDES --------------------*/
+#include "stdafx.h"
+#include "SharedIncludes.h"
+
+/*#include <OgreCamera.h>
+#include <OgreEntity.h>
+#include <OgreMesh.h>
+#include <OgreSubMesh.h>
+#include <OgreLogManager.h>
+#include <OgreRoot.h>
+#include <OgreViewport.h>
+#include <OgreSceneManager.h>
+#include <OgreRenderWindow.h>
+#include <OgreConfigFile.h>
+#include <OgreManualObject.h>*/
+
+//#define DBOUT( s ) { std::ostringstream os_; os_ << s; OutputDebugString( os_.str().c_str() );  }
+
+class MeshDeformer {
+public:
+	MeshDeformer(void);
+
+	void collisonDeform(Ogre::SceneNode *vehicle, const Ogre::Vector3 &epicentre);
+	void MeshDeformer::traceNodeHierarchy(Ogre::SceneNode *rootnode);
+	~MeshDeformer(void);
+
+protected :
+	virtual void deformMesh(
+		const Ogre::MeshPtr    mesh,
+		const Ogre::Vector3    &epicentre,
+        const Ogre::Vector3    &position   =  Ogre::Vector3::ZERO,
+        const Ogre::Quaternion &orient     =  Ogre::Quaternion::IDENTITY,
+		const Ogre::Vector3    &scale      =  Ogre::Vector3::UNIT_SCALE
+	);
+
+	virtual Ogre::ManualObject* drawLine(Ogre::SceneManager* mSceneMgr, Ogre::Vector3 &start, Ogre::Vector3 &end);
+
+	virtual unsigned time_seed();
+};
+
+#endif __MESHDEFORMER_H_
