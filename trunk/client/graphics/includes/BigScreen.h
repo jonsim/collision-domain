ee58a745e47135e2f6ba01759dd5451ad9bdcfea
ash568@gmail.com <ash568@gmail.com@876d4c38-226f-9f3a-54bf-000a6e7cd450>
2012-04-14 15:53:45 +0000

    Adding missing file. Sorry

---
+++ trunk/client/graphics/includes/BigScreen.h
@@ -0,0 +1,52 @@
+/**
+ * @file	NetworkCore.h
+ * @brief 	Takes notifications and deals with them however networking needs to
+ */
+#ifndef BIGSCREEN_H
+#define BIGSCREEN_H
+
+/*-------------------- INCLUDES --------------------*/
+#include "stdafx.h"
+#include "GameIncludes.h"
+
+#include "RakNetTypes.h"
+#include <vector>
+#include "RakNetTime.h"
+#include "GetTime.h"
+#include <sstream>
+
+class BigScreen
+{
+public:
+								BigScreen();
+	bool						declareNewPlayer( RakNet::RakNetGUID playerid ); //Returns true if the player has been added to big screen
+	void						manageNewPlayer(Player* player);
+	void						addCamera(Ogre::Camera* cam);
+	void						setupMapView();
+	void						updateMapView();
+	void						hideScreen();
+	void						showScreen();
+	void						handlePowerups();
+	void						manageNewPowerup(Powerup* pu);
+private:
+	//ViewportManager*			mViewportManager;
+	std::vector<ViewCamera*>	viewCameraVector;
+	Ogre::OverlayContainer*		olcMap;
+	Ogre::OverlayElement*		oleCar;
+	Ogre::Vector3				mapCorner;
+	Ogre::Vector3				mapSize;
+
+	Ogre::OverlayElement*		oleVIP1;
+	Ogre::OverlayElement*		oleVIP2;
+
+	void						updatePlayer(Player* player, Ogre::OverlayElement* carOverlay);
+	
+	void						setMapCorner(Ogre::Vector3 corner);
+	void						setMapSize(Ogre::Vector3 size);
+
+	inline float				convertWorldToScreenX(float xPos);
+	inline float				convertWorldToScreenY(float yPos);
+
+};
+
+#endif // #ifndef BIGSCREENH
