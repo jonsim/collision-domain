019fe2b1013c808f8990e7f8ff8c09e418af0a28
jamie01 <jamie01@876d4c38-226f-9f3a-54bf-000a6e7cd450>
2012-04-20 21:50:55 +0000

    Putting back a couple of things lost with the merge

--- trunk/shared/physics/cars/SmallCar.cpp
+++ trunk/shared/physics/cars/SmallCar.cpp
@@ -167,8 +167,6 @@ SmallCar::SmallCar(int uniqueCarID, TeamID tid)
 /// @brief  Destructor to clean up. Doesn't currently remove the car from the physics world though.
 SmallCar::~SmallCar(void)
 {
-    GameCore::mPhysicsCore->getWorld()->getBroadphase()->getOverlappingPairCache()->cleanProxyFromPairs( mCarChassis->getBroadphaseHandle(), GameCore::mPhysicsCore->getWorld()->getDispatcher() );
-    
     GameCore::mPhysicsCore->removeBody( mCarChassis );
     GameCore::mPhysicsCore->removeBody( mLDoorBody );
     GameCore::mPhysicsCore->removeBody( mRDoorBody );
