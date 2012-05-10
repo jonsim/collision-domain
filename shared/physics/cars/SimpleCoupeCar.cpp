7aa5fe2a4c6d89fde36b073968b6c48735101731
ash568 <ash568@gmail.com>
2012-05-10 15:31:56 +0000

    Dead icons are also now in. Nice icon Jonny.

--- shared/physics/cars/SimpleCoupeCar.cpp
+++ shared/physics/cars/SimpleCoupeCar.cpp
@@ -408,6 +408,10 @@ void SimpleCoupeCar::loadDestroyedModel (void)
     mChassisNode->detachAllObjects();
     createGeometry("CarEntity_Burnt", "banger_burnt.mesh", mChassisNode, false);
     makeBitsFallOff();
+    #ifdef COLLISION_DOMAIN_CLIENT
+        if(GameCore::mClientGraphics->getGraphicsState() == PROJECTOR)            
+            GameCore::mClientGraphics->mBigScreen->changeArrow(this->getUniqueID(),3);
+    #endif
 }
 
 
