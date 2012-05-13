609834d1ee4a9b0e91fc4d0a0205bc7c859de447
JonathanSimmonds <JonathanSimmonds@gmail.com>
2012-05-13 19:16:29 +0000

    Fixed the issue mentioned in Note 1 in r602 myself - the correct colour of car is now shown depending on the next round (along with the correct title text). Also changed the shrapnel: it is now smaller, less of it is emitted, it is emitted less far, it now lasts for just 2.5 seconds, fades out twice as fast and no longer has the spark effects. I only found out that most people weren't happy with the shrapnel (among other things) today: if people don't like it just fucking tell me, I can't guess whether you like it or not and I have usually spent so much time looking at it I don't have a good handle on it.

--- client/base/Player.cpp
+++ client/base/Player.cpp
@@ -240,8 +240,8 @@ void Player::angleTest(void) {
     bool isFront = ((damageSection == 0) || (damageSection == 1));
     TeamID tid = (GameCore::mGameplay->getGameMode() == FFA_MODE) ? NO_TEAM : getTeam();
     float shrapnelPlaneOffset = (getCarType() == CAR_SMALL) ? -0.75f : ((getCarType() == CAR_BANGER) ? -0.85f : -1.25f);
-    float shrapnelCount    = damage / 5.0f;
-    float shrapnelMaxSpeed = damage / 10.0f;
+    float shrapnelCount    = damage / 10.0f;
+    float shrapnelMaxSpeed = damage / 20.0f;
 
     if(damage > 40.f) getCar()->removeCarPart(damageSection);
     switch(crashType) {
